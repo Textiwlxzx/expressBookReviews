6146e4d91b56984a9caf58cd5b2bae27ffceb80a
Tiw <93968340+Textiwlxzx@users.noreply.github.com>
2026-09-04 03:48:51 +0700

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,81 +1,113 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
-const regd_users = express.Router();
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+const axios = require('axios');
 
-let users = [];
-
-const isValid = (username) => {
-  let userswithsamename = users.filter((user) => {
-    return user.username === username;
-  });
-  return userswithsamename.length > 0;
-};
-
-const authenticatedUser = (username, password) => {
-  let validusers = users.filter((user) => {
-    return (user.username === username && user.password === password);
-  });
-  return validusers.length > 0;
-};
-
-// Login as a registered user
-regd_users.post("/login", (req, res) => {
+public_users.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
   if (!username || !password) {
-    return res.status(404).json({ message: "Error logging in" });
+    return res.status(404).json({ message: "Username and password are required" });
   }
 
-  if (authenticatedUser(username, password)) {
-    let accessToken = jwt.sign({ username: username }, 'access', { expiresIn: 60 * 60 });
-
-    req.session.authorization = {
-      accessToken,
-      username
-    };
-    return res.status(200).json({ message: "User logged in successfully", token: accessToken });
-  } else {
-    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  if (isValid(username)) {
+    return res.status(404).json({ message: "User already exists!" });
   }
+
+  users.push({ "username": username, "password": password });
+  return res.status(200).json({ message: "User successfully registered. Now you can login" });
 });
 
-// Add or update a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  const isbn = req.params.isbn;
-  const review = req.query.review;
-  const username = req.user.username;
+// Get the book list available in the shop
+public_users.get('/', function (req, res) {
+  res.send(JSON.stringify(books, null, 4));
+});
 
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+});
+
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  let matchingBooks = [];
+  Object.keys(books).forEach((key) => {
+    if (books[key].author === author) {
+      matchingBooks.push(books[key]);
+    }
+  });
+  res.send(matchingBooks);
+});
+
+// Get all books based on title
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  let matchingBooks = [];
+  Object.keys(books).forEach((key) => {
+    if (books[key].title === title) {
+      matchingBooks.push(books[key]);
+    }
+  });
+  res.send(matchingBooks);
+});
+
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
   if (!books[isbn]) {
     return res.status(404).json({ message: "Book not found" });
   }
-
-  if (!review) {
-    return res.status(400).json({ message: "Review text is required" });
-  }
-
-  books[isbn].reviews[username] = review;
-  return res.status(200).json({ message: `Review for ISBN ${isbn} added/updated by ${username}` });
+  res.send(books[isbn].reviews);
 });
 
-// Delete a book review
-regd_users.delete("/auth/review/:isbn", (req, res) => {
-  const isbn = req.params.isbn;
-  const username = req.user.username;
+// ---- Task 10-13: Same operations implemented with Axios (Promise / async-await) ----
 
-  if (!books[isbn]) {
-    return res.status(404).json({ message: "Book not found" });
+// Task 10: Get all books – async/await
+async function getAllBooks() {
+  try {
+    const response = await axios.get("http://localhost:5000/");
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
   }
+}
 
-  if (books[isbn].reviews[username]) {
-    delete books[isbn].reviews[username];
-    return res.status(200).json({ message: `Review for ISBN ${isbn} deleted` });
-  } else {
-    return res.status(404).json({ message: "No review found for this user to delete" });
+// Task 11: Search by ISBN – Promises
+function getBookByISBN(isbn) {
+  return axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch((error) => console.error(error.message));
+}
+
+// Task 12: Search by author – async/await
+async function getBooksByAuthor(author) {
+  try {
+    const response = await axios.get(`http://localhost:5000/author/${encodeURIComponent(author)}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
   }
-});
+}
 
-module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
-module.exports.users = users;
+// Task 13: Search by title – async/await
+async function getBooksByTitle(title) {
+  try {
+    const response = await axios.get(`http://localhost:5000/title/${encodeURIComponent(title)}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+module.exports.general = public_users;
