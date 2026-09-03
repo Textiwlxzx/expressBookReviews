5acd384fb8dfcb7e6d6e80459aceb3e8fe997b91
Tiw <93968340+Textiwlxzx@users.noreply.github.com>
2026-09-04 03:39:05 +0700

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
 app.use(express.json());
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }));
 
-app.use("/customer", session({secret: "fingerprint_customer", resave: true, saveUninitialized: true}))
-
-app.use("/customer/auth/*", function auth(req, res, next) {
+app.use("/customer/auth/*splat", function auth(req, res, next) {
   if (req.session.authorization) {
     let token = req.session.authorization['accessToken'];
-
     jwt.verify(token, "access", (err, user) => {
       if (err) {
         return res.status(403).json({ message: "User not authenticated" });
